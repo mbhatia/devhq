52a290c37d245cb27e52961ee496eeac83b77511
Manuj Bhatia <manuj@manujbhatia.net>
2026-05-21 23:01:40 -0500

    do not add duplicate view on reload

--- plugins/sivraj/init.lua
+++ plugins/sivraj/init.lua
@@ -1,11 +1,19 @@
 -- mod-version:3
 
 local core = require "core"
+
+for _, loaded_view in ipairs(core.root_view.root_node:get_children()) do
+  if loaded_view._sivraj_treeview then
+    return loaded_view
+  end
+end
+
 local TreeView = require "libraries.generic_treeview"
 local default_treeview = require "plugins.treeview"
-
 local view = TreeView()
 local node = core.root_view.root_node:get_node_for_view(default_treeview)
+
+view._sivraj_treeview = true
 view.node = node:split("left", view, { x = true }, true)
 
 return view
